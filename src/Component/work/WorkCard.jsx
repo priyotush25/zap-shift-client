6ad4733507e21cdd989e0fc11f23ada5239d3616
Priyotush <priyotushpaul40@gmail.com>
2025-11-21 00:58:41 +0600

    update work it

--- src/Component/work/WorkCard.jsx
+++ src/Component/work/WorkCard.jsx
@@ -1,13 +1,17 @@
 import bookIcon from "../../assets/bookingIcon.png";
+import Card from "../../shared/card.jsx/Card";
 
 const WorkCard = ({ item }) => {
   const { title, description } = item;
 
   return (
-    <div className="p-4 lg:p-8 rounded-3xl shadow-sm bg-white">
-      <img src={bookIcon} alt="" className="w-14" />
-      <h1 className="text-xl font-bold text-secondary mb-4 mt-6">{title}</h1>
-      <p>{description}</p>
+    <div>
+      <Card
+        className={"bg-white"}
+        title={title}
+        description={description}
+        img={bookIcon}
+      />
     </div>
   );
 };
--- src/shared/card.jsx/Card.jsx
+++ src/shared/card.jsx/Card.jsx
@@ -1,9 +1,10 @@
-const Card = () => {
+const Card = ({ img, title, description, className, children }) => {
   return (
-    <div className="p-8 rounded-3xl shadow-sm">
-      {/* <img src={img} alt="" />
+    <div className={`${className} p-8 rounded-3xl shadow-sm`}>
+      <img src={img} alt="" />
       <h1 className="text-xl font-bold text-secondary mb-4 mt-6">{title}</h1>
-      <p>{para}</p> */}
+      <p>{description}</p>
+      {children && <div className="mt-4">{children}</div>}
     </div>
   );
 };
